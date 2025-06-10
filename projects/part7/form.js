b05802844c20c8ec24bad27e2de8af04f0a02060
brionnas <brionnaswinton@gmail.com>
2025-06-10 12:37:40 -0400

    pushed saved changes

--- projects/part7/form.js
+++ projects/part7/form.js
@@ -10,10 +10,10 @@ document.getElementById("contactForm").addEventListener("submit", async function
   if (name && email && message) {
     const formData = new FormData();
 
-    // ✅ Web3Forms access key
+    
     formData.append("access_key", "be2b2acf-4b64-4ed1-831f-c78b88df116d");
 
-    // ✅ Append form field values
+    
     formData.append("name", name);
     formData.append("email", email);
     formData.append("message", message);
